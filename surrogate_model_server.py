721bdbe0dafd6b4128ecfc3890bbdcceed4cbdb3
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-07-31 15:15:28 -0400

    MNT: Suppress tensorflow warnings

--- surrogate_model_server.py
+++ surrogate_model_server.py
@@ -2,13 +2,14 @@ from lume_epics.epics_server import Server
 from surrogate_model import Model
 from lume_model.utils import load_variables
 import argparse
+import tensorflow as tf
+
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--no-monitor', help='Run server without monitor.', action='store_false', dest="monitor", default=True)
 args = parser.parse_args()
 monitor = args.monitor
-
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 variable_file = "files/surrogate_model_variables.pickle"
 input_variables, output_variables = load_variables(variable_file)
