a3ba2f4c06859d883b6e3fa6637e715bc42a315b
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-08-03 21:14:36 -0400

    MNT: Update build to use bokeh serve

---
+++ bokehserverextension.py
@@ -0,0 +1,7 @@
+
+from subprocess import Popen
+
+
+def load_jupyter_server_extension(nbapp):
+    """serve the bokeh-app directory with bokeh server"""
+    Popen(["bokeh", "serve", "surrogate_model_client.py", "--allow-websocket-origin=*"])
