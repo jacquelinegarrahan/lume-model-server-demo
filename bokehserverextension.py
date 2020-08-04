2b82d9f02e680c3ecb41615c6dfd1457402ec8e4
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-08-04 10:58:16 -0400

    Correct bokeh serve path

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -3,4 +3,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "bokeh-app", "--allow-websocket-origin=*"])
+    Popen(["bokeh", "serve", "surrogate_model_client.py", "--allow-websocket-origin=*"])
