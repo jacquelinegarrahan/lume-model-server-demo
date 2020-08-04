1beba1a3088f722059c3e0aaca051dbd0fa01e9e
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-08-03 22:15:46 -0400

    Remove server extension

--- bokehserverextension.py
+++
@@ -1,7 +0,0 @@
-
-from subprocess import Popen
-
-
-def load_jupyter_server_extension(nbapp):
-    """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "surrogate_model_client.py", "--allow-websocket-origin=*"])
