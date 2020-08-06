d7cf2a5619d99f491f9194a77b71756563e936e1
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2020-08-06 15:41:02 -0400

    MNT: Update start and extension

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -1,7 +1,9 @@
-
 from subprocess import Popen
-
+import os
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["bokeh", "serve", "surrogate_model_client.py", "--allow-websocket-origin=*"])
+    env = os.environ.copy()
+    env["EPICS_CA_ADDR_LIST"]="0.0.0.0"
+    env["EPICS_PVA_ADDR_LIST"]="0.0.0.0"
+    Popen(["bokeh", "serve", "surrogate_model_client.py", "--allow-websocket-origin=*"], env=env)
